651bcb54f410b4a4aa8f640502ce0b0b98b10fe2
Elias Bernardo <ebmm01@gmail.com>
2018-04-20 21:32:14 -0300

    O código agora lê o arquivo e salva numa matriz de ponteiros

--- EDA/Arquivo.c
+++ EDA/Arquivo.c
@@ -3,22 +3,52 @@
 
 int main(int argc, char const *argv[]) {
 
-  int n,status;
-  char *c;
-  FILE *file;
-  file = fopen("asphalt_01.txt", "r");
+  int i,j,
+      linha=0, //contador de linhas.
+      coluna=1, // contador de colunas. Iniciado em 1 pois não há ';' final.
+      **mat; //declaração da matriz (ponteiro de ponteiro).
 
-  if(file == NULL){
-     printf("Can not open file!\n");
+  char letra;
+  FILE *arquivo;
+  arquivo = fopen("test.txt", "r");
+
+//verifica se o arquivo está vazio.
+  if(arquivo == NULL){
+     printf("Erro: arquivo em branco!\n");
   }
-fscanf(file, "%d%c", &n, c);
-  while(!feof(file)){
-      printf("%d", n);
-       fscanf(file, "%d%c", &n, c);
 
-
+while((letra=fgetc(arquivo))!=EOF){
+      if (letra=='\n'){
+        linha++;
+      }
+      else if (linha==1 && letra == ';'){
+        coluna++;
+      }
   }
-  fclose(file);
+
+rewind(arquivo);
+
+  mat = (int**)malloc(linha*sizeof(int *));
+  for(i = 0; i<linha; i++){
+    *(mat+i) = (int*)malloc(coluna*sizeof(int));}
+
+    for (i=0;i<linha;i++){
+      for (j=0;j<coluna;j++){
+         fscanf(arquivo,"%d%c", (*(mat+i)+j),&letra);
+      }
+  }
+
+    for (i=0;i<linha;i++){
+      for (j=0;j<coluna;j++){
+        printf(" %d", *(*(mat+i)+j));
+      }
+      printf("\n");
+  }
+
+
+//Lembrar de liberar a memória da matriz (free)
+  printf("Linhas: %d \n Colunas: %d", i,j);
+  fclose(arquivo);
 
   return 0;
 }
