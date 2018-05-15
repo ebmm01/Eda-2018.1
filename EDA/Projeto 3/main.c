2755048325b4a462d07442e2ba54359046a28e30
Elias Bernardo <ebmm01@gmail.com>
2018-05-14 23:41:02 -0300

    Adição da leitura e conversão do arquivo em uma lista

--- EDA/Projeto 3/main.c
+++ EDA/Projeto 3/main.c
@@ -6,21 +6,20 @@
 void menu();
 
 int main(int argc, char const *argv[]) {
-  printf("TESTE");
   FILE *arquivo;
   int c, linhas=0;
   char escolha= '0', letra;
   int x;
   Lista *li;
   li = cria_lista();
-  menu();
 
   //Daqui pra baixo são meio que testes pra ver se funciona, apenas isso
   struct Pessoa *dados_pessoa;
-
+  char name[100];
   //pessoa 1
   dados_pessoa = (struct Pessoa*)malloc(sizeof(struct Pessoa));
-  dados_pessoa->nome = "Egzona";
+  scanf(" %[^\n]s", name);
+  dados_pessoa->nome = name;
   dados_pessoa->Celular = "12345678";
   dados_pessoa->Endereco = "Rua de sei la onde";
   dados_pessoa->cep = 123445656;
@@ -43,8 +42,7 @@ int main(int argc, char const *argv[]) {
   dados_pessoa->Endereco = "123123123123";
   dados_pessoa->cep = 1222222;
   insere_lista_ordenada(li, *dados_pessoa);
-  imprimir(li);
-  Libera_lista(li);
+  //Libera_lista(li);
 
   //Funções da biblioteca
   /*int x = insere_lista_inicio(li, dados_pessoa);
@@ -57,16 +55,58 @@ int main(int argc, char const *argv[]) {
   int x = consulta_lista_cep(li, posicao, &dados_pessoa);*/
 
    //parte do arquivo, não implementada ainda
-   arquivo = fopen("contatos.txt", "r");
-    char linha1[100];
 
-     while((letra=fgetc(arquivo))!=EOF){
-          if (letra=='\n' ){
-            linhas++;
-          }
-      }
+    FILE *fp = fopen("contatos.txt", "r");
 
-printf("%d\n",linhas );
+  while((letra=fgetc(fp))!=EOF){
+       if (letra=='\n' ){
+         linhas++;
+       }
+   }
+
+printf("\n%d\n",linhas );
+rewind(fp);
+char (*lines)[100] = NULL; /* pointer to array of type char [MAXC] */
+ int i, n = 0;
+    if (fp == NULL) {  /* valida o arquivo para leitura */
+        fprintf (stderr, "error: file open failed'.\n");
+        return 1;
+    }
+
+    if (!(lines = malloc (linhas * sizeof *lines))) { /* alloca o max de arrays */
+        fprintf (stderr, "error: virtual memory exhausted 'lines'.\n");
+        return 1;
+    }
+
+    while (n < linhas && fgets (lines[n], 100, fp)) { /* lê cada linha */
+        char *p = lines[n];                  /* designa o ponteiro */
+        for (; *p && *p != '\n'; p++) {}     /* encontra o 1º '\n'  */
+        *p = 0, n++;                         /* nul-termiante  */
+    }
+    if (fp != stdin) fclose (fp);   /* fecha o arquivo se ! stdin */
+
+    /* exibe linhas */
+    for (i = 0; i < n; i++) printf (" line[%2d] : '%s'\n", i + 1, lines[i]);
+dados_pessoa = (struct Pessoa*)malloc(sizeof(struct Pessoa));
+for (i = 1; i < 5; i++){
+  if(i==0+1)
+    dados_pessoa->nome =lines[0] ;
+  if(i==1+1)
+    dados_pessoa->Celular = lines[i];
+  if(i==2+1)
+    dados_pessoa->Endereco = lines[i];
+  if(i==4){
+    int num1 = atoi(lines[3]);
+
+    dados_pessoa->cep =num1 ;}
+}
+insere_lista_ordenada(li, *dados_pessoa);
+imprimir(li);
+
+free(dados_pessoa);
+    free (lines);   /* free allocated memory */
+
+
 
 //menu incompleto
  /*while(escolha!='5'){
