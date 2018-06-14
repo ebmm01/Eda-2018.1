5747172794091f37cbab17a75259005dff769121
Elias Bernardo <ebmm01@gmail.com>
2018-06-14 14:33:48 -0300

    Adicionando função de impressão

--- EDA/Projeto 5/abin.h
+++ EDA/Projeto 5/abin.h
@@ -9,6 +9,25 @@ struct NO{
 
 typedef struct NO* ArvBin;
 
+typedef struct BSTNode_struct BSTNode;
+
+struct BSTNode_struct {
+    BSTNode *ptLeft, *ptRight;
+
+    //length of the edge from this node to its children
+    int edge_length;
+
+    int height;
+
+    int element;
+
+    //-1=I am left, 0=I am root, 1=right
+    int parent_dir;
+
+    //max supported unit32 in dec, 10 digits max
+    char label[11];
+};
+
 ArvBin* cria_ArvBin();
 
 void libera_ArvBin(ArvBin *raiz);
@@ -36,6 +55,26 @@ struct NO* remove_atual(struct NO *atual);
 int consulta_ArvBin(ArvBin *raiz, int valor);
 
 ArvBin* loadTreeFromFile(char *name);
+
+void printElements(ArvBin *t);
+
+BSTNode *build_ascii_tree_recursive(ArvBin *t);
+
+BSTNode *build_ascii_tree(ArvBin *t);
+
+void free_ascii_tree(BSTNode *node);
+
+void compute_lprofile(BSTNode *node, int x, int y);
+
+void compute_rprofile(BSTNode *node, int x, int y);
+
+void filledge(BSTNode *node);
+
+void printLevel(BSTNode *node, int x, int level);
+
+int MIN(int X, int Y);
+
+int MAX(int X, int Y);
 
 #endif
 /* Chamada das funções
--- EDA/Projeto 5/main.c
+++ EDA/Projeto 5/main.c
@@ -4,10 +4,11 @@
 
 int  main(int argc, char const *argv[]) {
   ArvBin* raiz = cria_ArvBin();
-  raiz = loadTreeFromFile("bst1");
+  raiz = loadTreeFromFile("bst2");
   printf("Testando a raiz:\n");
   emOrdem_ArvBin(raiz);
   printElements(raiz);
   libera_ArvBin(raiz);
+
   return 0;
 }
